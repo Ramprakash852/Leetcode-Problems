a65aea7a83f470912f9b88eec18ab92b859bd9a1
Bhukya Ramprakash <165421848+Ramprakash852@users.noreply.github.com>
2026-03-31 18:46:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        //Kadane’s Algorithm
+
+        int maxProfit = 0, currentProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            currentProfit = Math.max(0, currentProfit + prices[i] - prices[i-1]);
+            maxProfit = Math.max(maxProfit, currentProfit);
+        }
+
+        return maxProfit;
+    }
+}
