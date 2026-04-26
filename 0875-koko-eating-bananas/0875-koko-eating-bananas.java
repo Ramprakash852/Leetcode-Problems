2f8d1278dd48101fc017b44a3d47bf610738477e
Bhukya Ramprakash <165421848+Ramprakash852@users.noreply.github.com>
2026-04-26 16:00:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 1000000000;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (check(piles, mid) <= h) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    
+    public int check(int[] piles, int k) {
+        int hours = 0;
+        for (int p: piles) {
+            hours += p/ k;
+            if (p % k > 0) hours++;
+        }
+        return hours;
+    }
+}
